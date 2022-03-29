aa61f64589610bc42bffed188626c29c4d8ba619
jordiett <jlincar@epsa.upv.es>
2022-03-29 16:23:42 +0200

    Finished Practice 3

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -3,13 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:ipc_jlincar_p1/model/list_products.dart';
 import 'product_details_page.dart';
-import 'dart:math';
 
 class ListPage extends StatelessWidget {
   const ListPage({Key? key}) : super(key: key);
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -18,34 +14,44 @@ class ListPage extends StatelessWidget {
           title: const Text('Products'),
           backgroundColor: Colors.indigo,
         ),
-        body: FutureList());
+        body: const FutureList());
   }
 }
 
 class FutureList extends StatelessWidget {
-  FutureList({Key? key}) : super(key: key);
+  const FutureList({Key? key}) : super(key: key);
 
-  var random = Random();
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: DefaultAssetBundle.of(context).loadString("assets/json/products.json"),
-      builder: (context, AsyncSnapshot<String> snapshot){
-        if(snapshot.hasData){
+      future: DefaultAssetBundle.of(context)
+          .loadString("assets/json/products.json"),
+      builder: (context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.hasData) {
           Map<String, dynamic> data = json.decode(snapshot.data!);
           var products = Products.fromJson(data);
           final list = products.products.map((products) {
             return Column(children: [
               const Divider(),
               ListTile(
-                trailing: const Icon(Icons.chevron_right, color: Colors.grey, size: 50.0),
+                trailing: const Icon(Icons.chevron_right,
+                    color: Colors.grey, size: 50.0),
                 title: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(products.name),
-                      Text("${products.price}€", style: const TextStyle(color: Colors.grey),),
+                      Text(
+                        "${products.price}€",
+                        style: const TextStyle(color: Colors.grey),
+                      ),
                     ]),
-                leading: Hero(tag: "image" + random.nextInt(1000).toString(), child: Image.asset(products.image, width: 100,height: 100,)),
+                leading: Hero(
+                    tag: "image_${products.image}",
+                    child: Image.asset(
+                      products.image,
+                      width: 100,
+                      height: 100,
+                    )),
                 onTap: () {
                   var route = MaterialPageRoute(
                     builder: (context) => ProductDetailsPage(
@@ -53,7 +59,7 @@ class FutureList extends StatelessWidget {
                       price: products.price,
                       description: products.description,
                       image: products.image,
-                      heroTag: "image" + random.toString() ,
+                      heroTag: "image_${products.image}",
                     ),
                   );
                   Navigator.of(context).push(route);
@@ -66,49 +72,10 @@ class FutureList extends StatelessWidget {
             itemCount: list.length,
             itemBuilder: (context, i) => list[i],
           );
-
-        }else{
+        } else {
           return const LinearProgressIndicator();
         }
       },
-
     );
   }
 }
-
-/*
-class MyList extends StatelessWidget {
-  const MyList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final list = products.products.map((products) {
-      return Column(children: [
-        const Divider(),
-        ListTile(
-          trailing: const Icon(Icons.chevron_right, color: Colors.grey, size: 50.0),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-            Text(products.name),
-            Text("${products.price}€", style: const TextStyle(color: Colors.grey),),
-          ]),
-          leading: Image.asset(products.imgPath),
-          onTap: () {
-            var route = MaterialPageRoute(
-              builder: (context) => ProductDetailsPage(product: , imageRoute: ,),
-            );
-            Navigator.of(context).push(route);
-          },
-        ),
-      ]);
-    }).toList();
-
-    return ListView.builder(
-      itemCount: list.length,
-      itemBuilder: (context, i) => list[i],
-    );
-  }
-}
-
-*/
--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -19,6 +19,7 @@ class ProductDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.indigo,
         title: const Text("Product Details"),
@@ -26,37 +27,34 @@ class ProductDetailsPage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        decoration: const BoxDecoration(
-          color: Colors.grey
-        ),
+        decoration: const BoxDecoration(color: Colors.grey),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Spacer(flex:2),
-          Hero(
-            tag: heroTag,
-            child: Container(
-              width: 360,
-              height: 400,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(40),
-                image: DecorationImage(
-                  image: AssetImage(image)
-                )
+            const Spacer(flex: 2),
+            Hero(
+              tag: heroTag,
+              child: Container(
+                width: 360,
+                height: 400,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(40),
+                    image: DecorationImage(image: AssetImage(image))),
               ),
             ),
-          ),
-            const Spacer(flex:2,),
+            const Spacer(
+              flex: 2,
+            ),
             Container(
                 width: double.infinity,
                 height: 200,
                 decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40),
-            ),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
                 ),
                 child: SizedBox(
                   width: 100,
@@ -64,82 +62,39 @@ class ProductDetailsPage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Spacer(flex: 1,),
+                      const Spacer(
+                        flex: 1,
+                      ),
                       Text(name,
                           style: const TextStyle(
                             fontSize: 30,
                             fontWeight: FontWeight.bold,
                           )),
-                      const Spacer(flex: 1,),
+                      const Spacer(
+                        flex: 1,
+                      ),
                       Text(price.toString() + "€",
                           style: const TextStyle(
                             fontSize: 30,
                             fontWeight: FontWeight.bold,
                           )),
-                      const Spacer(flex: 1,),
+                      const Spacer(
+                        flex: 1,
+                      ),
                       Text(description,
                           textAlign: TextAlign.center,
                           style: const TextStyle(
                             fontSize: 20,
-
                           )),
-                      const Spacer(flex: 1,),
+                      const Spacer(
+                        flex: 1,
+                      ),
                     ],
                   ),
-                )
-
-            )
-
+                ))
           ],
         ),
-      )
-
+      ),
     );
   }
 }
-/*
-//Almacena el estado
-class MyProgressWidget extends StatefulWidget {
-  const MyProgressWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyProgressWidgetState createState() => _MyProgressWidgetState();
-}
-
-//Almacena la info de cómo se pinta el widget
-class _MyProgressWidgetState extends State<MyProgressWidget> {
-  double progressValue = 0.0;
-  @override
-  Widget build(BuildContext context) {
-    return LinearProgressIndicator(
-      value: progressValue,
-    );
-  }
-}
-
-
-class FutureWidget extends StatelessWidget {
-  const FutureWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: helloDelayed("Marimila"),
-      builder: (context, AsyncSnapshot<String> snapshot){
-        if(snapshot.hasData){
-          return Text(snapshot.data!);
-        }else{
-          return LinearProgressIndicator();
-        }
-      },
-
-    );
-  }
-}
-
-Future<String> helloDelayed(String name){
-  return Future.delayed(
-    const Duration(seconds: 5),
-      () { return "Hello, $name"; },
-  );
-}*/
